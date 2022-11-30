bc19328d7ec2613cb8528ba06f7290da77ca8b87
AlbertHuang <r.albert.huang@gmail.com>
2022-11-30 15:23:10 +0800

    fix: next.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 const { withExpo } = require("@expo/next-adapter");
 const withPlugins = require("next-compose-plugins");
-const withTM = require("next-transpile-modules")(["react-native-web"]);
+const withTM = require("next-transpile-modules")([
+    "react-native-web",
+    "@ui-kitten/components"
+]);
 
 module.exports = withPlugins([withTM, withExpo], {});
