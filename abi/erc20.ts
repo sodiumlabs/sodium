3e387c14af64306b79dd9f7599bb57a3967811c4
AlbertHuang <r.albert.huang@gmail.com>
2022-12-12 10:50:22 +0800

    chore: Support erc20 transfer smaples

---
+++ abi/erc20.ts
@@ -0,0 +1,23 @@
+import { Transaction } from '@0xsodium/transactions';
+import { BigNumber } from 'ethers';
+import { ERC20__factory } from '../gen';
+
+export type ERC20Transfer = {
+    to: string,
+    amount: BigNumber
+}
+
+export const decodeTransaction = (tx: Transaction): ERC20Transfer => {
+    const erc20Interface = ERC20__factory.createInterface();
+    const result = erc20Interface.decodeFunctionData("transfer", tx.data);
+    return {
+        to: result[0],
+        amount: BigNumber.from(result[1])
+    }
+}
+
+export const checkIsERC20Transfer = (tx: Transaction): boolean => {
+    // 0x+keccak256("transfer(address,uint256)"").slice(0, 8)
+    const transferMethodPrefix = "0xa9059cbb";
+    return tx.data.toString().startsWith(transferMethodPrefix);
+}
