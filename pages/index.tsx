aa292b0108745f01b24e86349c96f109bb45002e
qqoo6789 <270949894@qq.com>
2023-02-20 20:51:30 +0800

    use tab

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,8 @@ import { showErrorModal } from '../lib/data/modal';
 import { isNavigationReadyAtom, navigationRef } from '../components/base/navigation';
 import NavigationInit from '../components/base/navigationInit';
 import * as SplashScreen from 'expo-splash-screen';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 import {
   CoinScreen,
   ConnectScreen,
@@ -41,7 +43,6 @@ import { initProjectSetting } from '../lib/data/project';
 import { AuthCallbackScreen } from '../components/screen/authCallbackScreen';
 import { StatusBar } from 'expo-status-bar';
 import { authAtom } from '../lib/data/authAtom';
-import { init as initWalletConnect } from '../lib/walletconnect';
 
 const queryClient = new QueryClient(
   {
@@ -62,6 +63,9 @@ const queryClient = new QueryClient(
   // }
 );
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+const SettingsStack = createNativeStackNavigator();
+const HomeStack = createNativeStackNavigator();
 
 if (
   Platform.OS === "android" &&
@@ -97,7 +101,7 @@ export default function App() {
     Promise.all(initPromises).then(() => {
       return asyncSession()
     }).then(() => {
-      
+
     }).then(() => {
       if (Platform.OS != "web") {
         SplashScreen.hideAsync();
@@ -116,7 +120,7 @@ export default function App() {
             <NavigationInit />
             <BarUI />
             {/* screenOptions={{ headerShown: Platform.OS != 'web' }} */}
-            <Stack.Navigator screenOptions={{ headerShown: false, animation: 'none' }}  >
+            {/* <Stack.Navigator screenOptions={{ headerShown: false, animation: 'none' }}  >
               <Stack.Screen name={Screens.Opening} component={OpeningScreen} />
               <Stack.Screen name={Screens.Login} component={LoginScreen} />
               <Stack.Screen name={Screens.Wallet} component={WalletScreen} />
@@ -133,10 +137,37 @@ export default function App() {
               <Stack.Screen name={Screens.RecoveryCode} component={RecoveryCodeScreen} />
               <Stack.Screen name={Screens.Allowance} component={AllowanceScreen} />
               <Stack.Screen name={Screens.AuthCallbackScreen} component={AuthCallbackScreen} />
-            </Stack.Navigator>
+            </Stack.Navigator> */}
+
+
+            <Tab.Navigator screenOptions={{ headerShown: false, tabBarShowLabel: false, tabBarStyle: { display: 'none' } }}   >
+              <Tab.Screen name="Wallet" >
+                {() => (
+                  <SettingsStack.Navigator screenOptions={{ headerShown: false, animation: 'none' }}  >
+                    <SettingsStack.Screen name={Screens.Opening} component={OpeningScreen} />
+                    <SettingsStack.Screen name={Screens.Login} component={LoginScreen} />
+                    <SettingsStack.Screen name={Screens.Wallet} component={WalletScreen} />
+                    <SettingsStack.Screen name={Screens.Setting} component={SettingScreen} />
+                    <SettingsStack.Screen name={Screens.Profile} component={ProfileScreen} />
+                    <SettingsStack.Screen name={Screens.Session} component={SessionScreen} />
+                    <SettingsStack.Screen name={Screens.History} component={HistoryScreen} />
+                    <SettingsStack.Screen name={Screens.Coin} component={CoinScreen} />
+                    <SettingsStack.Screen name={Screens.Send} component={SendScreen} />
+                    <SettingsStack.Screen name={Screens.Deposit} component={DepositScreen} />
+                    <SettingsStack.Screen name={Screens.Connect} component={ConnectScreen} />
+                    <SettingsStack.Screen name={Screens.Security} component={SecurityScreen} />
+                    <SettingsStack.Screen name={Screens.SetupAuth} component={SetupAuthScreen} />
+                    <SettingsStack.Screen name={Screens.RecoveryCode} component={RecoveryCodeScreen} />
+                    <SettingsStack.Screen name={Screens.Allowance} component={AllowanceScreen} />
+                    <SettingsStack.Screen name={Screens.AuthCallbackScreen} component={AuthCallbackScreen} />
+                  </SettingsStack.Navigator>
+                )}
+              </Tab.Screen>
+            </Tab.Navigator>
+
           </NavigationContainer>
         </QueryClientProvider>
       </ApplicationProvider>
-    </SafeAreaProvider>
+    </SafeAreaProvider >
   );
 }
