55897812d2b34b6b5ce1852d4f6a28d8a0db4150
qqoo6789 <270949894@qq.com>
2023-02-20 18:51:47 +0800

    optimize modal

--- components/base/baseModal.tsx
+++ components/base/baseModal.tsx
@@ -1,10 +1,9 @@
 import { useEffect, useMemo, useRef, useState } from 'react';
-import { Animated, Dimensions, Easing, Modal, Pressable, StyleSheet, TouchableWithoutFeedback, ViewProps } from 'react-native';
+import { Animated, BackHandler, Dimensions, Easing, Modal, StyleSheet, TouchableWithoutFeedback, ViewProps } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { fixWidth } from '../../lib/define';
 import { useAdapterWeb } from '../../lib/hook/adapter';
-import { IconModalClose } from '../../lib/imageDefine';
-import MImage from '../baseUI/mImage';
+import { useDimensionSize } from '../../lib/hook/dimension';
 import { ModalCloseButton } from '../baseUI/modalCloseButton';
 import MVStack from '../baseUI/mVStack';
 
@@ -13,24 +12,75 @@ export const BaseModal = (props: ViewProps & { visible?: boolean, isFullScreen?:
 
   let { visible, hideModal, isFullScreen, isAnim = true, contentHeight = screenHeight, contentStyle = {} } = props;
   let marginTop = Math.max(screenHeight - contentHeight, 100);
-  const [isStartHide, setIsStartHide] = useState(false);
+  // const [isStartHide, setIsStartHide] = useState(false);
+  const [dimensionW, dimensionH] = useDimensionSize();
 
   const backgroundFadeAnim = useRef(new Animated.Value(0)).current;
+  const backgroundPosAnim = useRef(new Animated.Value(0)).current;
   const isAdapterWeb = useAdapterWeb();
   const insets = useSafeAreaInsets();
+
+  // 'visible' is the player request to hide and show
+  // 'uiVisible' is the real hiding and display of Modal Windows
+  const [uiVisible, setUiVisible] = useState(false);
+  const [isTweening, setIsTweening] = useState(false);
+
+  const showAnim = () => {
+    if (uiVisible) return;
+    if (isTweening) return;
+    setIsTweening(true);
+    setUiVisible(true);
+    backgroundPosAnim.setValue(dimensionH);
+    // console.log("show");
+
+    Animated.timing(backgroundFadeAnim, {
+      easing: Easing.quad,
+      toValue: 0.15,
+      duration: 300,
+      useNativeDriver: false
+    }).start();
+
+    Animated.timing(backgroundPosAnim, {
+      easing: Easing.quad,
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: false
+    }).start(() => {
+      setIsTweening(false);
+    });
+  }
+
+  const hideAnim = () => {
+    if (!uiVisible) return;
+    if (isTweening) return;
+    setIsTweening(true);
+    // console.log("hide");
+    // debugger
+    // console.log("hidehidehidehidehide");
+    Animated.timing(backgroundFadeAnim, {
+      easing: Easing.quad,
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: false
+    }).start();
+
+    Animated.timing(backgroundPosAnim, {
+      easing: Easing.quad,
+      toValue: dimensionH,
+      duration: 300,
+      useNativeDriver: false
+    }).start(() => {
+      hideModal();
+      setUiVisible(false);
+      setIsTweening(false);
+    });
+  }
+
   useEffect(() => {
-    // console.log(size[1]);
     if (!visible) {
-      backgroundFadeAnim.setValue(0);
-      setIsStartHide(false);
+      hideAnim();
     } else {
-      Animated.timing(backgroundFadeAnim, {
-        easing: Easing.quad,
-        toValue: 0.15,
-        duration: 200,
-        delay: 150,
-        useNativeDriver: false
-      }).start();
+      showAnim();
     }
   }, [visible]);
 
@@ -53,38 +103,45 @@ export const BaseModal = (props: ViewProps & { visible?: boolean, isFullScreen?:
   }, [isAdapterWeb, marginTop]);
 
   const onBackClick = () => {
-    setIsStartHide(true);
-    // backgroundFadeAnim.setValue(0);
-    setTimeout(() => {
-      hideModal();
-    }, 20);
+    hideAnim();
   }
 
+  useEffect(() => {
+    // if (!visible) return;
+    const hardwareBackPress = (): boolean => {
+      onBackClick();
+      return true;
+    }
+    BackHandler.addEventListener("hardwareBackPress", hardwareBackPress);
+    return BackHandler.removeEventListener("hardwareBackPress", hardwareBackPress);
+  }, [])
+
   return (
+    // <MVStack stretchH stretchW style={{ backgroundColor: 'transparent' }} >
+
     <Modal
       transparent={true}
-      animationType={isAnim ? 'slide' : 'none'}
-      visible={visible}
+      // animationType={isAnim ? 'slide' : 'none'}
+      visible={uiVisible}
     // presentationStyle='pageSheet'
     >
 
       <MVStack stretchW stretchH style={{ justifyContent: 'center', alignItems: 'center' }} >
 
-        {!isStartHide && (
-          <TouchableWithoutFeedback onPress={onBackClick}>
-            <Animated.View style={{ position: 'absolute', width: '100%', height: '100%', top: 0, left: 0, backgroundColor: 'rgb(0,0,0)', opacity: backgroundFadeAnim }}>
-            </Animated.View>
-          </TouchableWithoutFeedback>
-        )}
+        <TouchableWithoutFeedback onPress={onBackClick}>
+          <Animated.View style={{ position: 'absolute', width: '100%', height: '100%', top: 0, left: 0, backgroundColor: 'rgb(0,0,0)', opacity: backgroundFadeAnim }}>
+          </Animated.View>
+        </TouchableWithoutFeedback>
 
-        <MVStack stretchW stretchH style={[styles.content, contentStyle, adapterStyle]}>
+        <Animated.View style={[styles.content, contentStyle, adapterStyle, { transform: [{ translateY: backgroundPosAnim }] }]} >
           {
             props.children
           }
           <ModalCloseButton onClose={onBackClick} />
-        </MVStack>
+        </Animated.View>
       </MVStack>
     </Modal>
+    // </MVStack>
   );
 };
 
