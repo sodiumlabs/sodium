94535a6f520f4451073e6001fd81fd8766fe6d07
qqoo6789 <270949894@qq.com>
2023-02-24 16:20:05 +0800

    connect modal ui fit

--- components/modal/connectModal.tsx
+++ components/modal/connectModal.tsx
@@ -1,12 +1,14 @@
-import { ScrollView, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { useProjectSetting } from '../../lib/data/project';
 import { IConnectModalParam, IModalParam } from '../../lib/define';
+import { eColor } from '../../lib/globalStyles';
 import { useModalLoading } from '../../lib/hook/modalLoading';
 import { BaseModal } from '../base/baseModal';
+import { Spacer } from '../base/spacer';
+import MButton from '../baseUI/mButton';
+import { MButtonText } from '../baseUI/mButtonText';
 import MText from '../baseUI/mText';
 import MVStack from '../baseUI/mVStack';
-import { OperateBtnItem } from './modalItem/operateBtnItem';
-import { fixWidth } from '../../lib/define';
 
 export const ConnectModal = (props: { hideModal: () => void, modalParam: IModalParam<IConnectModalParam> }) => {
   const { modalParam, hideModal } = props;
@@ -33,18 +35,27 @@ export const ConnectModal = (props: { hideModal: () => void, modalParam: IModalP
       hideImmediately={modalParam.hideImmediately}
       isFullScreen={projectSetting.isBeOpenedByThirdParty}
       isAnim={!projectSetting.isBeOpenedByThirdParty}
+      contentHeight={500}
     >
-      <MVStack stretchW style={{ alignItems: 'center', flex: 1 }}>
-        <MVStack stretchW style={[styles.marginV, { flex: 1 }]}>
-          <ScrollView style={{ paddingHorizontal: 15 }}>
-            <MVStack stretchW stretchH>
-              <MText numberOfLines={null} style={{ marginVertical: 20, textAlign: 'center', fontWeight: '700' }}>
-                Do you want to allow {param?.options?.origin} to connect to your wallet?
-              </MText>
-            </MVStack>
-          </ScrollView>
-          <OperateBtnItem onCancelClick={onCancelClick} onConfirmClick={onConfirmClick} isConfirmLoading={isLoading} isConfirmEnable={true} />
+
+      <MVStack stretchH stretchW style={{ 'alignItems': 'center', padding: 15 }}>
+        <MText style={{ fontWeight: '700', textAlign: 'center' }} numberOfLines={null}>Do you want to allow {param?.options?.origin} to connect to your wallet?</MText>
+        <Spacer />
+        <MVStack stretchW style={{ alignItems: 'center' }}>
+          <MButton stretchW style={{ height: 30 }} onPress={onCancelClick} >
+            <MButtonText title={"Cancel"} />
+          </MButton>
+
+          <MButton stretchW style={{ backgroundColor: eColor.Blue, marginTop: 10, height: 30 }} onPress={onConfirmClick} isLoading={isLoading} >
+            <MButtonText title={"Confirm"} />
+          </MButton>
         </MVStack>
+
+        {
+          projectSetting.isBeOpenedByThirdParty && <Spacer />
+        }
+
+
       </MVStack>
     </BaseModal>
   );
