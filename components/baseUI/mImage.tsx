041508bac8a42d6298e74f5671145093633ba9c2
AlbertHuang <r.albert.huang@gmail.com>
2023-03-07 08:10:24 +0800

    fix: react native image type error

--- components/baseUI/mImage.tsx
+++ components/baseUI/mImage.tsx
@@ -1,13 +1,12 @@
 import Image from 'next/image';
-import { ImageProps } from "react-native";
+import { ImageProps, ImageSourcePropType } from "react-native";
 import MHStack from './mHStack';
 
-export default function MImage(props: ImageProps & { w?: number, h?: number, uri?: string }) {
+export default function MImage(props: Omit<ImageProps, 'source'> & { w?: number, h?: number, uri?: string, source?: ImageSourcePropType }) {
   let { uri, w = 12, h = 12, source, style } = props;
   if (!source && !uri) {
     return <MHStack style={{ width: w, height: h }}></MHStack>
   }
-
   return (
     <MHStack style={style}>
       <Image width={w} height={h} src={uri || source as string} alt={""} />
