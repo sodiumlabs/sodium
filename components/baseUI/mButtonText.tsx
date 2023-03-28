2c5eef0e0d94cbb6a168be0bbab692c942499eae
qqoo6789 <270949894@qq.com>
2023-03-28 17:05:12 +0800

    text no selected

--- components/baseUI/mButtonText.tsx
+++ components/baseUI/mButtonText.tsx
@@ -5,6 +5,6 @@ import { TextProps } from 'react-native';
 export const MButtonText = (props: TextProps & { title: string } & { fontSize?: number }) => {
   const { title, style, fontSize, ...reset } = props;
   return (
-    <MText style={[{ color: '#ffffff', fontWeight: '700' }, style]} {...reset} fontSize={fontSize}>{title}</MText>
+    <MText selectable={false} style={[{ color: '#ffffff', fontWeight: '700' }, style]} {...reset} fontSize={fontSize}>{title}</MText>
   )
 }
