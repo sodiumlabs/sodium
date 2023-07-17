6ca7c85a4ef6316d0693037cf19d315539a2b12f
AlbertHuang <r.albert.huang@gmail.com>
2023-07-17 10:27:32 +0800

    online auth service

--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -1,7 +1,8 @@
 import { AuthService } from './rpc';
 
 export const getAuthService = () => {
-    return new AuthService("http://127.0.0.1:8080", global.fetch);
+    // return new AuthService("http://127.0.0.1:8080", global.fetch);
+    return new AuthService("https://auth-service.melandworld.com", global.fetch);
 }
 
 export * from './rpc';
